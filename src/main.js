7464ea6bd15c6fdbbdd2268fed44d2ad327b917e
dona <windofwind@gmail.com>
2020-11-18 20:06:18 +0900

    part5

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import HelloWorldScene from './scenes/HelloWorldScene';
 import GameScene from "./scenes/GameScene";
 
 const config = {
-	type: Phaser.AUTO,
+	type: Phaser.CANVAS || Phaser.AUTO,
 	width: 800,
 	height: 600,
 	physics: {
